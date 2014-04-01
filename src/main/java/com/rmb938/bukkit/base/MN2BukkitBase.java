5d71a1215cde5bd59de99a2b9700ac689359fbde
rmb938 <rmb1993@gmail.com>
2014-03-31 20:19:33 -0400

    yay maven stuff :D

---
+++ src/main/java/com/rmb938/bukkit/base/MN2BukkitBase.java
@@ -0,0 +1,15 @@
+package com.rmb938.bukkit.base;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MN2BukkitBase extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
